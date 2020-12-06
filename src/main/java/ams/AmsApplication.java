2dc0bde489dc4b781c088bb8018ba556d3a0f714
BOL <15120022843@126.com>
2020-12-06 23:26:26 +0800

    测试

--- src/main/java/ams/AmsApplication.java
+++ src/main/java/ams/AmsApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 创建启动类
  *
  * 看这里
+ *
+ * 1231312313123213123123123123
+ *
+ * 刘创22222222222222222222222222222222B
  */
 @SpringBootApplication
 public class AmsApplication {
