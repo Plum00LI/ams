a766032c8fc5fed73a44bb46d97558c9516ad5a7
BOL <15120022843@126.com>
2020-12-06 23:15:30 +0800

    测试

--- src/main/java/ams/AmsApplication.java
+++ src/main/java/ams/AmsApplication.java
@@ -3,6 +3,11 @@ package ams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 创建启动类
+ *
+ * 看这里
+ */
 @SpringBootApplication
 public class AmsApplication {
 
